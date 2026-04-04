4709496696af950b35d3c55770fd4cb80aacff41
vicenzorm <vimasera11@gmail.com>
2026-04-04 17:38:27 -0300

    feat(SafeConsUnitTests): add unit tests for SafeCons

---
+++ SafeConsUnitTests/SafeConsUnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  SafeConsUnitTests.swift
+//  SafeConsUnitTests
+//
+//  Created by Vicenzo Másera on 04/04/26.
+//
+
+import Testing
+
+struct SafeConsUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
